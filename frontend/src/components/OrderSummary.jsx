555d37cfa1b022709bad824df879870b83708fc5
Ranjith H K <git@github.com:ranjith0903/mern.git>
2025-02-11 13:33:49 +0530

    erro in order summary

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51QBqViHVrEESOxZHwP3ur6Ga3060peMawzOmlj7qrPi5IWPLdVRHtUjyudYiXT874RgzwpKtYu46QyRsJkHqaF8C00heb4jEYZ"
 );
 
 const OrderSummary = () => {
